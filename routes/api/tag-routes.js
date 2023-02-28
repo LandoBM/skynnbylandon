f03b7c0ebe72f22ed2d1f1d59cd2ff4caa96dd33
Landon Murray <landonbmurray@icloud.com>
2023-02-28 00:15:05 -0500

    Final commit, updated README and fixed Tags update

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -47,7 +47,7 @@ router.put('/:id', async (req, res) => {
         id: req.params.id
       }
     })
-    res.status(200).json*(tagUpdate)
+    res.status(200).json(tagUpdate)
   }catch (err){
     res.status(500).json(err)
   }
